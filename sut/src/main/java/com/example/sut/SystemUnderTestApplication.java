836c574cd77dfc6a50b9d8a171dfbeaa3d057126
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2018-07-13 08:26:12 +0300

    [SUT] added @SpringBootApplication annotation

--- sut/src/main/java/com/example/sut/SystemUnderTestApplication.java
+++ sut/src/main/java/com/example/sut/SystemUnderTestApplication.java
@@ -2,14 +2,13 @@ package com.example.sut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class SystemUnderTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
